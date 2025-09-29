84bfc6af6cdcc9ec57ea43b13013c015ac7dc04c
Sharon <sharonsharon9048461583@gmail.com>
2025-09-29 20:39:12 +0530

    removed unwanted comments added for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client" 
 
-// We need to re-import the necessary components after resolving the definition issue
 import { Hero } from "../components/Hero"
 import { Projects } from "../components/Projects"
 import { About } from "../components/About"
